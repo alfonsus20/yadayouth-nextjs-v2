5703e3a97dec8bfe91dc01b237466645e4d20306
alfonsus20 <alfonschandrawan@gmail.com>
2022-04-30 20:18:01 +0700

    test: add article card unit test

---
+++ components/Card/Article/article.spec.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ArticleCard } from "..";
+import { render, screen } from "@testing-library/react";
+
+const mockedProps = {
+  imageURL: "http://google.com",
+  title: "Title test",
+  preview: "Preview test",
+};
+
+describe("Article Card unit test", () => {
+  it("should render correctly", () => {
+    render(<ArticleCard {...mockedProps} />);
+    expect(screen.getByTestId("card-image")).toBeInTheDocument();
+    expect(screen.getByTestId("card-title")).toBeInTheDocument();
+    expect(screen.getByTestId("card-description")).toBeInTheDocument();
+  });
+
+  it("should render title and description correctly", () => {
+    render(<ArticleCard {...mockedProps} />);
+    expect(screen.getByTestId("card-title").textContent).toEqual("Title test");
+    expect(screen.getByTestId("card-description").textContent).toEqual("Preview test");
+  });
+});
