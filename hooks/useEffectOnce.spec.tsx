5a26e0cbf429c797cc13d3570f5561706b7ca30d
alfonsus20 <alfonschandrawan@gmail.com>
2022-06-24 09:36:11 +0700

    test: renew useEffectOnce test case

--- hooks/useEffectOnce.spec.tsx
+++ hooks/useEffectOnce.spec.tsx
@@ -1,11 +1,29 @@
-import { renderHook } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { useState } from "react";
 import useEffectOnce from "./useEffectOnce";
 
 const mockedCallback = jest.fn();
 
+const TestComponent = () => {
+  useEffectOnce(mockedCallback);
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button
+        data-testid="btn-count"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        Button {count}
+      </button>
+    </div>
+  );
+};
+
 describe("useEffectOnce hook unit test", () => {
-  it("should call callback function only once", () => {
-    renderHook(() => useEffectOnce(mockedCallback));
+  it("should call callback function only once even though the component's state is updated", () => {
+    render(<TestComponent />);
+    fireEvent.click(screen.getByTestId("btn-count"));
     expect(mockedCallback).toHaveBeenCalledTimes(1);
   });
 });
